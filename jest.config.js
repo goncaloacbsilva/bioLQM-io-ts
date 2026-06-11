65b2ce1a18065ecf89f472334018cbf115904b2b
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-06-11 21:19:15 +0100

    fix: build on prepare

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
-  moduleFileExtensions: ["ts", "js", "json"],
-  moduleNameMapper: {
-    "^mddlib-js$": "<rootDir>/node_modules/mddlib-js/src/index.ts"
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!(mddlib-js)/)"
-  ]
+  moduleFileExtensions: ["ts", "js", "json"]
 };
